137bf285b4bc9af7d9271b0a865cff220d236c9c
amitetrogy-learning <amitetrogy@gmail.com>
2026-01-05 07:51:50 +0000

    feat: add swagger to comments route

--- src/express/comments/router.ts
+++ src/express/comments/router.ts
@@ -12,15 +12,347 @@ import { wrapController } from "../../utils/express/middlewares";
 
 const commentRouter = Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Comments
+ *   description: API endpoints for comments
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Comment:
+ *       type: object
+ *       properties:
+ *         _id:
+ *           type: string
+ *           description: Unique ID of the comment
+ *         commentText:
+ *           type: string
+ *           description: Content of the comment
+ *         postId:
+ *           type: string
+ *           description: ID of the post
+ *         senderId:
+ *           type: string
+ *           description: ID of the sender
+ *         createdAt:
+ *           type: string
+ *           format: date-time
+ *           description: Timestamp of when the comment was created
+ *         updatedAt:
+ *           type: string
+ *           format: date-time
+ *           description: Timestamp of when the comment was last updated
+ *
+ *     CreateComment:
+ *       type: object
+ *       required:
+ *         - commentText
+ *         - postId
+ *         - senderId
+ *       properties:
+ *         commentText:
+ *           type: string
+ *           description: Content of the comment
+ *           example: "This is a test comment"
+ *         postId:
+ *           type: string
+ *           description: ID of the post
+ *           example: "67a1d205c689f9a4e5476a1a"
+ *         senderId:
+ *           type: string
+ *           description: ID of the sender
+ *           example: "67a1d205c689f9a4e5476a1b"
+ *
+ *     UpdateComment:
+ *       type: object
+ *       properties:
+ *         commentText:
+ *           type: string
+ *           description: Content of the comment
+ *           example: "This is a test comment"
+ *         postId:
+ *           type: string
+ *           description: ID of the post
+ *           example: "67a1d205c689f9a4e5456a1a"
+ *         senderId:
+ *           type: string
+ *           description: ID of the sender
+ *           example: "67a1d205c689f9a4e5476a1c"
+ */
+
+/**
+ * @swagger
+ * /comments:
+ *   get:
+ *     summary: Get all comments
+ *     tags: [Comments]
+ *     responses:
+ *       '200':
+ *         description: A list of comments
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Comment'
+ *       '500':
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Error message
+ *                   example: "Internal server error"
+ */
 commentRouter.get("/", wrapController(CommentController.getAllComments));
+
+/**
+ * @swagger
+ * /comments/:id:
+ *   get:
+ *     summary: Get a comment by ID
+ *     tags: [Comments]
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: ID of the comment
+ *         example: "67a1d205c689f9a4e5476a1b"
+ *     responses:
+ *       '200':
+ *         description: A comment by ID
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Error message
+ *                   example: "Internal server error"
+ *       '400':
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Error message
+ *                   example: "Invalid comment ID"
+ *       '500':
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Error message
+ *                   example: "Internal server error"
+ */
 commentRouter.get("/:id", ValidateRequest(getCommentByIdSchema), wrapController(CommentController.getCommentById));
+
+/**
+ * @swagger
+ * /comments/post/:postId:
+ *   get:
+ *     summary: Get comments by post ID
+ *     tags: [Comments]
+ *     parameters:
+ *       - name: postId
+ *         in: path
+ *         required: true
+ *         description: ID of the post
+ *         example: "67a1d205c689f9a4e5476a1a"
+ *     responses:
+ *       '200':
+ *         description: A list of comments
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Comment'
+ *       '400':
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Error message
+ *                   example: "Invalid post ID"
+ *       '500':
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Error message
+ *                   example: "Internal server error"
+ */
 commentRouter.get(
     "/post/:postId",
     ValidateRequest(getCommentsByPostIdSchema),
     wrapController(CommentController.getCommentsByPostId)
 );
+
+/**
+ * @swagger
+ * /comments:
+ *   post:
+ *     summary: Create a new comment
+ *     tags: [Comments]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/CreateComment'
+ *     responses:
+ *       '201':
+ *         description: Comment created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Comment'
+ *       '400':
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Error message
+ *                   example: "Invalid request body"
+ *       '500':
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Error message
+ *                   example: "Internal server error"
+ */
 commentRouter.post("/", ValidateRequest(createCommentSchema), wrapController(CommentController.createComment));
+
+/**
+ * @swagger
+ * /comments/:id:
+ *   put:
+ *     summary: Update a comment by ID
+ *     tags: [Comments]
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: ID of the comment
+ *         example: "67a1d205c689f9a4e5476a1b"
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/UpdateComment'
+ *     responses:
+ *       '200':
+ *         description: Comment updated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Comment'
+ *       '400':
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Error message
+ *                   example: "Invalid request body"
+ *       '500':
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Error message
+ *                   example: "Internal server error"
+ */
 commentRouter.put("/:id", ValidateRequest(updateCommentSchema), wrapController(CommentController.updateCommentById));
+
+/**
+ * @swagger
+ * /comments/:id:
+ *   delete:
+ *     summary: Delete a comment by ID
+ *     tags: [Comments]
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: ID of the comment
+ *         example: "67a1d205c689f9a4e5476a1b"
+ *     responses:
+ *       '200':
+ *         description: Comment deleted successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Error message
+ *                   example: "Comment <id> deleted successfully"
+ *       '400':
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Error message
+ *                   example: "Invalid comment ID"
+ *       '500':
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Error message
+ *                   example: "Internal server error"
+ */
 commentRouter.delete(
     "/:id",
     ValidateRequest(deleteCommentByIdSchema),
